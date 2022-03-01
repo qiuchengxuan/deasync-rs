54df851757943a3cbfc2c271b43ac221faecedf0
qiuchengxuan <qiuchengxuan@gmail.com>
2022-03-01 19:56:51 +0800

    Implement deasync and remove unnessesary code

--- src/parse.rs
+++ src/parse.rs
@@ -22,9 +22,6 @@ impl Parse for Item {
         let attrs = input.call(Attribute::parse_outer)?;
         let mut fork;
         let item = if let Some(mut item) = fork!(fork = input).parse::<ItemImpl>().ok() {
-            if item.trait_.is_none() {
-                return Err(Error::new(Span::call_site(), "expected a trait impl"));
-            }
             item.attrs = attrs;
             Item::Impl(item)
         } else if let Some(mut item) = fork!(fork = input).parse::<ItemTrait>().ok() {
@@ -34,10 +31,7 @@ impl Parse for Item {
             item.attrs = attrs;
             Item::Fn(item)
         } else {
-            return Err(Error::new(
-                Span::call_site(),
-                "expected trait impl, trait or fn",
-            ));
+            return Err(Error::new(Span::call_site(), "expected impl, trait or fn"));
         };
         input.advance_to(&fork);
         Ok(item)
